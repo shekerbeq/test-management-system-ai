ecc91119165a93fc6d1722ce20ed141e1761d24c
shekerbeq <dexmaxxx@mail.ru>
2026-04-26 22:13:25 +0500

    test: AuthController саттi тексерiлдi (тiркелу жане кiру)

--- user-module/src/main/java/kz/testmanagement/user/service/UserService.java
+++ user-module/src/main/java/kz/testmanagement/user/service/UserService.java
@@ -1,4 +1,26 @@
 package kz.testmanagement.user.service;
 
+import kz.testmanagement.core.entity.User;
+import kz.testmanagement.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Пайдаланушы осындай email-мен тіркелген: " + user.getEmail());
+        }
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
