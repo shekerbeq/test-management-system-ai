77c1f9a62d0435157a07560c2e348969fe6345ff
shekerbeq <dexmaxxx@mail.ru>
2026-05-01 20:22:38 +0500

    feat: TestConfigService қосылды (тест конфигурацияларын басқару)

---
+++ test-module/src/main/java/kz/testmanagement/test/service/TestConfigService.java
@@ -0,0 +1,31 @@
+package kz.testmanagement.test.service;
+
+import kz.testmanagement.test.entity.TestConfig;
+import kz.testmanagement.test.repository.TestConfigRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TestConfigService {
+
+    private final TestConfigRepository testConfigRepository;
+
+    public TestConfig save(TestConfig config) {
+        return testConfigRepository.save(config);
+    }
+
+    public Optional<TestConfig> findById(Long id) {
+        return testConfigRepository.findById(id);
+    }
+
+    public List<TestConfig> findByCreatorId(Long creatorId) {
+        return testConfigRepository.findByCreatorId(creatorId);
+    }
+
+    public void deleteById(Long id) {
+        testConfigRepository.deleteById(id);
+    }
+}
