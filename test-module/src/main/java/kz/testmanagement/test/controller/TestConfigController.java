f16bd86d0c69c48d5e0178c239512f57a98dd559
shekerbeq <dexmaxxx@mail.ru>
2026-05-01 21:38:26 +0500

    тест тапсыру толық циклы тексерілді, нәтижелер дұрыс көрсетіледі

--- test-module/src/main/java/kz/testmanagement/test/controller/TestConfigController.java
+++ test-module/src/main/java/kz/testmanagement/test/controller/TestConfigController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tests")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('TEACHER')")
 public class TestConfigController {
 
     private final TestConfigService testConfigService;
