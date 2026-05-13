0fc9e060b9d9f91912345d92bb58581c0dd8133c
akoha <dexmaxxx@mail.ru>
2026-05-14 03:50:12 +0500

    feat: RegisterRequest DTO қосылды

--- user-module/src/main/java/kz/testmanagement/user/dto/RegisterRequest.java
+++ user-module/src/main/java/kz/testmanagement/user/dto/RegisterRequest.java
@@ -1,4 +1,20 @@
 package kz.testmanagement.user.dto;
 
+import kz.testmanagement.core.entity.Role;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String name;       // фронтенд шлёт "name"
+    private String email;
+    private String password;
+    private String role;       // "ROLE_STUDENT" или "ROLE_TEACHER"
+
+    public Role getRoleEnum() {
+        if (role == null) return Role.STUDENT;
+        if (role.startsWith("ROLE_")) {
+            return Role.valueOf(role.substring(5));   // убираем "ROLE_"
+        }
+        return Role.valueOf(role);
+    }
 }
